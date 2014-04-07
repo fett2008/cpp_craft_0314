c22e60795db9f5b9b708557cbe26f20bd875385b
Litvinov_Gleb <fett2008@yandex.ru>
2014-04-07 23:51:35 +0300

    week4

---
+++ solutions/gleb_litvinov/4/sources/4_5/solution.cpp
@@ -0,0 +1,47 @@
+#include "solution.h"
+
+task4_5::solution::solution( const data_type& data )
+{
+	this->data=data;
+}
+
+void task4_5::solution::start() const
+{
+	if (data.size()==0)
+	{
+		min=max=0;
+		return;
+	}
+	boost::thread_group t;
+	for (int i=0;i<data.size();i++)
+		t.create_thread( boost::bind(&task4_5::solution::solve,this,data[i]));
+	t.join_all();
+}
+void task4_5::solution::solve(const std::vector<int> &tmp) const
+{
+	int min_ = std::numeric_limits< int >().max();
+	int max_= std::numeric_limits< int >().min();
+	for (int i=0;i<tmp.size();++i)
+	{
+		if (tmp[i]>max_)max_=tmp[i];
+		if (tmp[i]<min_)min_=tmp[i];
+	}
+	{
+		boost::mutex::scoped_lock lock( mtx );
+		if(max_>max) max=max_;
+		if (min_<min) min=min_;
+	}
+}
+
+int task4_5::solution::get_min() const
+{
+	min = std::numeric_limits< int >().max();
+	start();
+	return min;
+}
+int task4_5::solution::get_max() const
+{
+	max= std::numeric_limits< int >().min();
+	start();
+	return max;
+}
---
+++ solutions/gleb_litvinov/4/sources/4_5/solution.h
@@ -0,0 +1,27 @@
+#ifndef _TASK4_5_SOLUTION_H_
+#define _TASK4_5_SOLUTION_H_
+
+#include <vector>
+#include <boost/thread.hpp>
+#include <limits>
+
+namespace task4_5
+{
+	typedef std::vector< std::vector< int > > data_type;
+
+	class solution
+	{
+		mutable boost::mutex  mtx;
+		data_type data;
+		mutable int max,min;
+	public:
+		explicit solution( const data_type& data );
+		void start() const;
+		void solve(const std::vector<int> &tmp)const ;
+		int get_min() const;
+		int get_max() const;
+	};
+}
+
+#endif // _TASK4_5_SOLUTION_H_
+
